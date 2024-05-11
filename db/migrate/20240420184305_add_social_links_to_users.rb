10e32e5de3278ca849175c1ac48ded0a18f083ea
jefferson joe mendoza vega <joe.mendoza.vega@gmail.com>
2024-05-10 20:28:25 -0500

    primero?

--- db/migrate/20240420184305_add_social_links_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddSocialLinksToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :facebook, :string
-    add_column :users, :whatsapp, :string
-    add_column :users, :telegram, :string
-  end
-end
